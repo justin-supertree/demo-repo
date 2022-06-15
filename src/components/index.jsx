55d799f3090401426c6dbac6e21a4f7529dda0b6
Justin_supertree <justin1688@supertree.co>
2022-06-15 16:24:34 +0900

    test 1

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import First from "./First";
+
+const Home = () => {
+  return (
+    <>
+      <First />
+      <div>Here is Home page</div>
+    </>
+  );
+};
+
+export default Home;
